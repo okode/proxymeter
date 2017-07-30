9e57d2766ce15961feb3c358950fb9c9d6bc7a4a
Pedro Jorquera <pjorquera@okode.com>
2017-07-30 18:29:18 +0200

    Playing with background modes

--- ios/Proxymeter/Proxymeter/AppDelegate.swift
+++ ios/Proxymeter/Proxymeter/AppDelegate.swift
@@ -13,13 +13,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
 
     var window: UIWindow?
     var deviceManager = ESTDeviceManager()
-
+    var beaconManager = ESTBeaconManager()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let temperatureNotification = ESTTelemetryNotificationTemperature { (temperature) in
-            print("Current temperature: \(temperature.temperatureInCelsius) C")
+        /*
+        let notifier = ESTTelemetryNotificationAmbientLight { (notification) in
+            print("Current value: \(notification.ambientLightLevelInLux)")
         }
-        deviceManager.register(forTelemetryNotification: temperatureNotification)
+        deviceManager.register(forTelemetryNotification: notifier)
+        */
+        
+        beaconManager.requestAlwaysAuthorization()
+        beaconManager.delegate = self
+        beaconManager.startRangingBeacons(in: CLBeaconRegion(proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 62703, minor: 48774, identifier: "myRegion"))
+        
         return true
+    }
+    
+    func beaconManager(_ manager: Any, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        print("Beacon/s ranged")
+    }
+    
+    func beaconManager(_ manager: Any, monitoringDidFailFor region: CLBeaconRegion?, withError error: Error) {
+        print("Monitoring failed")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -30,10 +46,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("Entering background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("Entering foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
