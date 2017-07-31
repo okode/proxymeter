3390274d487eb91da3164f4857700adb8c21a851
Pedro Jorquera <pjorquera@okode.com>
2017-08-01 01:08:22 +0200

    Added missing permissions

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -10,21 +10,23 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, private ibeacon: IBeacon) { }
 
-  ngOnInit() {
-    this.ibeacon.requestAlwaysAuthorization();
+  start() {
+    this.ibeacon.requestAlwaysAuthorization()
+    .then(() => console.log('Success auth'))
+    .catch(reason => console.log(`Fail: ${JSON.stringify(reason)}`));
     
     let delegate = this.ibeacon.Delegate();
     delegate.didEnterRegion().subscribe(
-      data => console.log(`Beacon Enter: ${data.beacons[0].uuid}`),
+      data => console.log('Beacon Enter'),
       error => console.error(error)
     );
 
     delegate.didExitRegion().subscribe(
-      data => console.log(`Beacon Exit: ${data.beacons[0].uuid}`),
+      data => console.log('Beacon Exit'),
       error => console.error(error)
     );
 
-    let beaconRegion = this.ibeacon.BeaconRegion('myRegion', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 62703, 48774);
+    let beaconRegion = this.ibeacon.BeaconRegion('MY_BEACON_REGION', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 62703, 48774);
     this.ibeacon.startMonitoringForRegion(beaconRegion)
     .then(data => console.log(`Beacon start monitoring region: ${data}`))
     .catch(error => console.log(`Beacon start monitoring error: ${error}`));
