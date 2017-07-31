4428bdf29247ed4844f6b2a0cefdfc4a142deae5
Pedro Jorquera <pjorquera@okode.com>
2017-07-31 09:58:10 +0200

    Added beacon enter / exit logic

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IBeacon } from '@ionic-native/ibeacon';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private ibeacon: IBeacon) { }
 
+  ngInitialize() {
+    this.ibeacon.requestAlwaysAuthorization();
+    
+    let delegate = this.ibeacon.Delegate();
+    delegate.didEnterRegion().subscribe(
+      data => console.log(`Beacon Enter: ${data.beacons[0].uuid}`),
+      error => console.error(error)
+    );
+
+    delegate.didExitRegion().subscribe(
+      data => console.log(`Beacon Exit: ${data.beacons[0].uuid}`),
+      error => console.error(error)
+    );
+
+    let beaconRegion = this.ibeacon.BeaconRegion('myRegion', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 62703, 48774);
+    this.ibeacon.startMonitoringForRegion(beaconRegion)
+    .then(data => console.log(`Beacon start monitoring region: ${data}`))
+    .catch(error => console.log(`Beacon start monitoring error: ${error}`));
   }
 
 }
